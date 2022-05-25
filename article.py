c8a80bf75380ff100aedcc262474113f1d12e888
double J, T <50047351+Powering111@users.noreply.github.com>
2022-05-25 17:41:29 +0900

    회사 목록(국내)

--- article.py
+++ article.py
@@ -22,10 +22,20 @@ driver.implicitly_wait(10)
 start = 1
 url = f'https://search.naver.com/search.naver?where=news&sm=tab_pge&query=%EB%A9%94%ED%83%80%EB%B2%84%EC%8A%A4&sort=0&photo=0&field=0&pd=0&ds=&de=&cluster_rank=247&mynews=0&office_type=0&office_section_code=0&news_office_checked=&nso=so:r,p:all,a:all&start={start}'
 
+def get_company_list():
+    company_list = list()
+    driver.get('https://ko.wikipedia.org/wiki/%EB%8C%80%ED%95%9C%EB%AF%BC%EA%B5%AD%EC%9D%98_%EA%B8%B0%EC%97%85_%EB%AA%A9%EB%A1%9D')
+    companys = driver.find_elements(By.TAG_NAME, 'li')
+    for company in companys:
+        company_list.append(company.text)
+    return company_list
+
 def get_article():
     article_links = list()
     driver.get(url)
     articles = driver.find_elements(By.CLASS_NAME, 'news-tit')
     for article in articles:
         title = article.text
-        if '참여' in title or '구축' in title or '사업' in title or
+
+if __name__ == '__main__':
+    print(get_company_list())
