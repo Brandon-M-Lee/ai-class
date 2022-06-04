4d5bee4add0bc167193701d811c38c0a5d41c954
Brandon-M-Lee <minjae.py@gmail.com>
2022-06-05 01:11:52 +0900

    주가 데이터 분석 시작

--- finance.py
+++ finance.py
@@ -1,5 +1,14 @@
 import os
+import datetime
 os.system('pip3 install -r requirements.txt')
 import FinanceDataReader as fdr
 import pandas as pd
+import matplotlib.pyplot as plt
 
+def get_data(date, code):
+    date = datetime.datetime.strptime(date, '%Y.%m.%d.')
+    delta = datetime.timedelta(days=10)
+    data = fdr.DataReader(code, date - delta, date + delta)
+    data['High'].plot()
+
+get_data('2022.04.11.', '005301')
